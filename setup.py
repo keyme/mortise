f29aee487f768df58454651e2014cf9082d24fbc
Jeff Ciesielski <jeff.ciesielski@key.me>
2018-10-31 15:32:48 -0400

    Fix topics and roll version to 1.0.1
    This is for our first deployment!

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.1',
+    version='1.0.1',
 
     description='Synchronous state machine framework for event driven systems',
     long_description=long_description,
@@ -44,11 +44,11 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: State Machine',
+        'Topic :: Software Development :: Libraries',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
