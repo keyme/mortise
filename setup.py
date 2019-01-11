bf24baf88de7bbd4a7a887ab3196022c9fb44370
Jeff Ciesielski <jeff.ciesielski@key.me>
2019-01-11 13:08:07 -0500

    Update to version 1.0.2
    Changes:
    * Fixed packaging to include testing module
    * Added ability to reset transitions and times

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1',
+    version='1.0.2',
 
     description='Synchronous state machine framework for event driven systems',
     long_description=long_description,
